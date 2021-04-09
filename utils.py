d6d7392cc9e0b6a1487495fa0bf06111d23fd2f3
thisissum <ThisIsSunMH@gmail.com>
2021-04-09 12:39:09 +0800

    delete vectorizer.py

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import re
-import jieba
 from typing import *
+
+import numpy as np 
+import jieba
+
 
 
 def sentence_tokenize(text: str, pattern: str = r"？|。|；|！"):
@@ -34,8 +37,23 @@ def drop_html(text: str, pattern: str=r"<.*?>"):
     output = [piece for piece in re.sub(pattern, " ", text).split(" ") if piece]
     return " ".join(output)
 
-def edit_distance(text1, text2):
-    pass
+def edit_distance(text1: str, text2: str):
+    dp = np.zeros((len(text1)+1, len(text2)+1))
+    # init dp
+    for i in range(len(text1)+1):
+        dp[i, 0] = i
+    for j in range(len(text2)+1):
+        dp[0, j] = j
+    
+    # state transformation
+    for i in range(1, len(text1)+1):
+        for j in range(1, len(text2)+1):
+            if text1[i-1] == text2[j-1]:
+                dp[i, j] = dp[i-1, j-1]
+            elif text1[i-1] != text2[j-1]:
+                dp[i, j] = min(dp[i-1, j], dp[i, j-1], dp[i-1, j-1]) + 1
+    
+    return dp[-1, -1]
 
 
 
